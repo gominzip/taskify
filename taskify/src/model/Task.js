3f77a7e44fd588f7f9520352fd8067e969899ac3
gominzip <ahaminji@naver.com>
2024-08-20 19:00:30 +0900

    feat: 모델 클래스 구현

---
+++ taskify/src/model/Task.js
@@ -0,0 +1,13 @@
+class Task {
+  constructor(id, columnId, title, description, author, createdAt, updatedAt) {
+    this.id = id;
+    this.columnId = columnId;
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.createdAt = createdAt || new Date().toISOString();
+    this.updatedAt = updatedAt || new Date().toISOString();
+  }
+}
+
+module.exports = Task;
