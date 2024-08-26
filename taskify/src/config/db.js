fae06da72a8f68f484e6d48a678b6aa17a60134c
gominzip <ahaminji@naver.com>
2024-08-26 19:57:16 +0900

    feat: .env로 환경변수 관리

--- taskify/src/config/db.js
+++ taskify/src/config/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const pool = mysql.createPool({
-  host: "192.168.64.3",
-  user: "myuser",
-  password: "mypassword",
-  database: "taskify",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
