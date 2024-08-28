c693a8a22f99a55ded799b346505a2c52ddf64b7
gominzip <ahaminji@naver.com>
2024-08-28 22:16:46 +0900

    chore: 불필요한 파일 제거

--- taskify/src/public/js/DOM/DOM.js
+++
@@ -1,8 +0,0 @@
-class DOM {
-  constructor(tagName, innerText, className) {
-    this.node = document.createElement(tagName);
-    this.node.innerText = innerText;
-    if (className) this.node.classList.add(className);
-  }
-}
-export default DOM;
