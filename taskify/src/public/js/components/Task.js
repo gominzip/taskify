77548150cb97734ec892a5306c8eef0bcdb503ed
gominzip <ahaminji@naver.com>
2024-08-28 23:49:27 +0900

    feat: 부분 렌더링 확인을 위한 테스트용 리프레시 버튼 구현

--- taskify/src/public/js/components/Task.js
+++ taskify/src/public/js/components/Task.js
@@ -1,11 +1,16 @@
 import Component from "../core/Component.js";
 
 export default class Task extends Component {
+  setup() {
+    this.state = { ...this.props }; // 초기상태를 props로
+  }
+
   template() {
-    const { id, title, description, userName } = this.props;
+    const { id, title, description, userName } = this.state;
 
     return `
      <div class="task-item" data-task-id="${id}">
+        <button class="task-refresh-btn">🔄</button>
         <button class="task-delete-btn">❌</button>
         <h4>${title}</h4>
         <p>${description}</p>
@@ -18,5 +23,22 @@ export default class Task extends Component {
     this.addEvent("click", ".task-delete-btn", async () => {
       this.props.deleteTask(this.props.id);
     });
+
+    this.addEvent("click", ".task-refresh-btn", async () => {
+      this.refreshTask();
+    });
+  }
+
+  async refreshTask() {
+    try {
+      this.setState({
+        id: this.props.id,
+        title: "부분 렌더링 테스트",
+        description: "되나?",
+        userName: "ㅇㅅㅇ",
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
