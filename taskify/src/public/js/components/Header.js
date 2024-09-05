f5f526d33febf3670244032bf2c67fbbcdafe4af
gominzip <ahaminji@naver.com>
2024-09-05 19:52:38 +0900

    feat: 정렬 상태를 관리하는 SortStore 추가

--- taskify/src/public/js/components/Header.js
+++ taskify/src/public/js/components/Header.js
@@ -1,22 +1,46 @@
+import { SORT_TEXT, SORT_TYPES } from "../constants/sortTypes.js";
 import Component from "../core/Component.js";
+import sortStore from "../stores/SortStore.js";
 
 export default class Header extends Component {
+  setup() {
+    this.state = {
+      sortTypes: Object.values(SORT_TYPES),
+      currentSortTypeIndex: 0,
+    };
+  }
+
   template() {
+    const sortText = this.getSortText();
     return `
         <div class="header-left-content">
           <p>TASKIFY</p>
           <button class="sort-btn">
             <span class="material-symbols-outlined">swap_vert</span>
-            <span class="sort-btn-text">생성 순</span>
+            <span class="sort-btn-text">${sortText}</span>
           </button>
         </div>
         <button id="history-btn" class="material-symbols-outlined">history</button>
     `;
   }
 
+  getSortText() {
+    const { currentSortTypeIndex, sortTypes } = this.state;
+    const sortType = sortTypes[currentSortTypeIndex];
+    return SORT_TEXT[sortType] || "생성 순";
+  }
+
   setEvent() {
     this.addEvent("click", ".sort-btn", this.handleSortClick.bind(this));
   }
 
-  handleSortClick() {}
+  handleSortClick() {
+    const { sortTypes, currentSortTypeIndex } = this.state;
+    const nextIndex = (currentSortTypeIndex + 1) % sortTypes.length;
+    const newSortType = sortTypes[nextIndex];
+
+    this.setState({ currentSortTypeIndex: nextIndex });
+
+    sortStore.setSortType(newSortType);
+  }
 }
---
+++ taskify/src/public/js/constants/sortTypes.js
@@ -0,0 +1,11 @@
+export const SORT_TYPES = {
+  CREATED_AT: "createdAt",
+  LATEST: "latest",
+  PRIORITY: "priority",
+};
+
+export const SORT_TEXT = {
+  createdAt: "생성 순",
+  latest: "최신 순",
+  priority: "중요도 순",
+};
