6390424543a6d181902c5a7e090e04173e982687
gominzip <ahaminji@naver.com>
2024-09-10 18:10:00 +0900

    feat: login 페이지 렌더 및 로그인 api 연결

---
+++ taskify/src/public/js/apis/userAPI.js
@@ -0,0 +1,47 @@
+import { handleAsync } from "../../utils/handleAsync.js";
+
+export async function login(username, password) {
+  return handleAsync(async () => {
+    const response = await fetch("/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      const errorMessage = errorData.message || "로그인 실패";
+      throw new Error(errorMessage);
+    }
+    const data = await response.json();
+    return data;
+  });
+}
+
+export async function logout() {
+  return handleAsync(async () => {
+    const response = await fetch("/user/logout", {
+      method: "POST",
+    });
+    if (!response.ok) throw new Error("로그아웃 실패");
+    const data = await response.json();
+    return data;
+  });
+}
+
+export async function getUserStatus() {
+  return handleAsync(async () => {
+    const response = await fetch("/user/status");
+    if (!response.ok) throw new Error("사용자 상태 조회 실패");
+    const data = await response.json();
+    return data;
+  });
+}
+
+export async function getUserInfo() {
+  return handleAsync(async () => {
+    const response = await fetch("/user/info");
+    if (!response.ok) throw new Error("사용자 정보 가져오기 실패");
+    const data = await response.json();
+    return data;
+  });
+}
